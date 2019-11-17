da1a4fcdcbbf8e9dc65192199da0c79de77a81a6
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-17 17:22:03 +0000

    feat(Dashboard): Added Missed Calls and Callbacks Notifications

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -26,3 +26,46 @@ navigator.wakeLock.request("system").then(
 
 //release() is used to release the lock.
 navigator.wakeLock.release("display");
+
+
+//Notifications
+
+function sendNotification(notification_text) {
+    if (!("Notification" in window)) {
+      console.log("This browser does not support system notifications");
+    }
+  
+    else if (Notification.permission === "granted") {
+      var notification = new Notification(notification_text);
+    }
+  
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        if (permission === "granted") {
+          var notification = new Notification(notification_text);
+        }
+      });
+    }
+}
+
+
+// Call Notifications
+
+function callNotifications() {
+    $.getJSON('/data/missed_calls.json', function (data) {
+        let count = object.data.length;
+        if (count > 0) {
+            sendNotification('Existem ' + count + ' Chamadas Perdidas');
+        }
+    });
+
+    $.getJSON('/data/callbacks.json', function (data) {
+        let count = object.data.length;
+        if (count > 0) {
+            sendNotification('Existem ' + count + ' Chamadas Por Devolver');
+        }
+    });
+}
+
+callNotifications();
+setInterval(callNotifications, 60 * 60 * 1000);
