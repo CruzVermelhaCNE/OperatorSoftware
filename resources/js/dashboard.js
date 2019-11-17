a10d2c7c19230e9ae8aa03468b718babdfc47d4a
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-17 16:12:57 +0000

    feat(Dashboard): Prevented OS from sleeping

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -2,3 +2,27 @@ feather = require('feather-icons');
 
 feather.replace();
 
+//Prevent Windows from Sleeping
+
+//navigator.wakeLock is the main standby API property.
+//request method requests the computer to not enter standby mode. Here "display" indicates that the monitor shouldn't enter standby mode.
+navigator.wakeLock.request("display").then(
+    function successFunction() {
+        // success
+    },
+    function errorFunction() {
+        // error
+    }
+);
+//here system indicates CPU, GPU, radio, wifi etc.
+navigator.wakeLock.request("system").then(
+    function successFunction() {
+        // success
+    },
+    function errorFunction() {
+        // error
+    }
+);
+
+//release() is used to release the lock.
+navigator.wakeLock.release("display");
