ccac78e9f18d38b127dccc376465a652ade9cbdf
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-25 16:11:59 +0000

    fix(Door Opener): Response issues

--- app/Http/Controllers/GDSAPI.php
+++ app/Http/Controllers/GDSAPI.php
@@ -21,6 +21,6 @@ class GDSAPI extends Controller
         $IDCode = $array["IDCode"];
         $AuthCode = md5($ChallengeCode . ":GDS3710IDyTIHwNgZ:". env('GDS3710_PASSWORD'));
         $url = "https://". env('GDS3710_IP') . "/jpeg/stream?type=1&stream=0&user=".env('GDS3710_USERNAME')."&authcode=".$AuthCode."&idcode=".$IDCode;
-        return response()->text($url);
+        return response($url);
     }
 }
