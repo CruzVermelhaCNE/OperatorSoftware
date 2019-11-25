e003e1d70a5334bf4053e84d1d069158ebcb51f0
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-25 17:11:28 +0000

    fix(Door System): Implementing toastr

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,9 +3,10 @@ window._ = require('lodash');
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
+    window.toastr = require('toastr');
 
     require('bootstrap');
-    require( 'datatables.net-bs4' );
+    require('datatables.net-bs4');
 
 } catch (e) {}
 
@@ -18,8 +19,6 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-window.toastr = require('toastr');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
