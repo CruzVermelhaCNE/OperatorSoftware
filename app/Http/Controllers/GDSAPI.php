dae8f7ac9a7f9f6bd152ca8a75486f0700b766f2
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-25 16:07:15 +0000

    fix(Door Opener): SSL Issues

--- app/Http/Controllers/GDSAPI.php
+++ app/Http/Controllers/GDSAPI.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class GDSAPI extends Controller
 {
     public function getVideoURL() {
-        $xml_string = file_get_contents("https://". env('GDS3710_IP') . "/jpeg/stream?type=0&stream=0&user=". env('GDS3710_USERNAME'));
+        $arrContextOptions=array(
+            "ssl"=>array(
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+            ),
+        );  
+        $xml_string = file_get_contents("https://". env('GDS3710_IP') . "/jpeg/stream?type=0&stream=0&user=". env('GDS3710_USERNAME'), false, stream_context_create($arrContextOptions));
         $xml = simplexml_load_string($xml_string);
         $json = json_encode($xml);
         $array = json_decode($json,true);
