7b94177d7144ef6691cac74b7ceda73c150746e2
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-25 16:11:04 +0000

    fix(Door Opener): fixing xml issues;

--- app/Http/Controllers/GDSAPI.php
+++ app/Http/Controllers/GDSAPI.php
@@ -17,8 +17,8 @@ class GDSAPI extends Controller
         $xml = simplexml_load_string($xml_string);
         $json = json_encode($xml);
         $array = json_decode($json,true);
-        $ChallengeCode = $array["Configuration"]["ChallengeCode"];
-        $IDCode = $array["Configuration"]["IDCode"];
+        $ChallengeCode = $array["ChallengeCode"];
+        $IDCode = $array["IDCode"];
         $AuthCode = md5($ChallengeCode . ":GDS3710IDyTIHwNgZ:". env('GDS3710_PASSWORD'));
         $url = "https://". env('GDS3710_IP') . "/jpeg/stream?type=1&stream=0&user=".env('GDS3710_USERNAME')."&authcode=".$AuthCode."&idcode=".$IDCode;
         return response()->text($url);
