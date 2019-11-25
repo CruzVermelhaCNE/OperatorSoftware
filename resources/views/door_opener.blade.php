ae80f356ea0f20527d85d4ef2ae6cc50dfb9b64c
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-25 16:21:42 +0000

    fix(Door Opener): Issues with CORS

--- resources/views/door_opener.blade.php
+++ resources/views/door_opener.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <a href="#" class="btn btn-primary">Abrir Porta</a>
         <div class="embed-responsive embed-responsive-16by9">
-            <video class="embed-responsive-item" id="door_opener_video" autoplay autobuffer src="https://192.168.2.90/videoview.html"></video>
+            <video class="embed-responsive-item" id="door_opener_video" autoplay autobuffer src="http://{{ env('GDS3710_IP') }}/videoview.html"></video>
         </div>
     </div>
 </main>
