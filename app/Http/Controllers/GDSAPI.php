6fa1f77f7750567213ee576d096c0dc7a80e7033
Miguel Santos <miguel.santos@computers.cvp-coimbra.org>
2019-11-25 16:05:47 +0000

    fix(Door Opener): Fixing Authentication

--- app/Http/Controllers/GDSAPI.php
+++ app/Http/Controllers/GDSAPI.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class GDSAPI extends Controller
 {
-    public function getAuthCode() {
-        $challenge_code = Input::get('cc');
-        $auth_code = md5($challenge_code . ":GDS3710IDyTIHwNgZ:". env('GDS3710_PASSWORD'));
-        return response()->text($auth_code);
+    public function getVideoURL() {
+        $xml_string = file_get_contents("https://". env('GDS3710_IP') . "/jpeg/stream?type=0&stream=0&user=". env('GDS3710_USERNAME'));
+        $xml = simplexml_load_string($xml_string);
+        $json = json_encode($xml);
+        $array = json_decode($json,true);
+        $ChallengeCode = $array["Configuration"]["ChallengeCode"];
+        $IDCode = $array["Configuration"]["IDCode"];
+        $AuthCode = md5($ChallengeCode . ":GDS3710IDyTIHwNgZ:". env('GDS3710_PASSWORD'));
+        $url = "https://". env('GDS3710_IP') . "/jpeg/stream?type=1&stream=0&user=".env('GDS3710_USERNAME')."&authcode=".$AuthCode."&idcode=".$IDCode;
+        return response()->text($url);
     }
 }
--- resources/views/door_opener.blade.php
+++ resources/views/door_opener.blade.php
@@ -4,7 +4,7 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 panel">
     <div class="container">
         <a href="#" class="btn btn-primary">Abrir Porta</a>
-        <div class="embed-responsive embed-responsive-16by9" id="door_opener_video">
+        <div class="embed-responsive embed-responsive-16by9">
             
         </div>
     </div>
@@ -14,21 +14,11 @@
 @section('javascript')
 @parent
 <script type="text/javascript">
-
 $(document).ready(() => {
-    $.ajax({type:"GET",
-            crossdomain: true,
-            url:"https://{{ env('GDS3710_IP')}}/jpeg/stream?type=0&user={{ env('GDS3710_USERNAME')}}",
-            dataType: "text/xml",
-            success: function(data){
-                let parser = new DOMParser();
-                let xmlDoc = parser.parseFromString(data,"text/xml");
-                let ChallengeCode = xmlDoc.getElementsByTagName("ChallengeCode")[0].nodeValue;
-                let IDCode = xmlDoc.getElementsByTagName("IDCode")[0].nodeValue;
-                 $.get("/data/gds_auth_code?cc="+ChallengeCode, function(AuthCode, status) {
-                    $('#door_opener_video').append("https://{{ env('GDS3710_IP')}}/jpeg/stream?type=1&user={{ env('GDS3710_USERNAME')}}&authcode="+AuthCode+"&idcode="+IDCode);
-                });
-            }
+    $(document).ready(() => {
+        $.get("/data/gds_video_url", function(url, status) {
+            $('#door_opener_video').append('<video class="embed-responsive-item" id="door_opener_video" autoplay autobuffer src="'+url+'"></video>');
+        });
     });
 });
 </script>
