b764a986c330077eff93e76a559d1449cf6f4fab
Miguel Santos <github@miguelsantos.tech>
2020-03-19 17:57:56 +0000

    fix(COVID19): Fix Events

--- app/Events/COVID19UpdateAmbulance.php
+++ app/Events/COVID19UpdateAmbulance.php
@@ -5,7 +5,7 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
