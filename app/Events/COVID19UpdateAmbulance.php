f3b53bedbdae71cfa9adb053d28bcdbbdbc5750f
Miguel Santos <github@miguelsantos.tech>
2020-03-19 17:48:55 +0000

    fix(COVID19): fix broadcasting

--- app/Events/COVID19UpdateAmbulance.php
+++ app/Events/COVID19UpdateAmbulance.php
@@ -32,7 +32,7 @@ class COVID19UpdateAmbulance implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new Channel('COVID19UpdateCase');
+        return new Channel('COVID19UpdateAmbulance');
     }
 
     /**
