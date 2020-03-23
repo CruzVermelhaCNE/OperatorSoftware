3c47bb0a3d7ff97eb8cd392524f7eb5bfb89ea76
Miguel Santos <github@miguelsantos.tech>
2020-03-23 14:00:59 +0000

    fix(COVID19): Ambulances

--- app/Http/Controllers/COVID19AmbulanceController.php
+++ app/Http/Controllers/COVID19AmbulanceController.php
@@ -26,7 +26,7 @@ class COVID19AmbulanceController extends Controller
     {
         $ambulances = COVID19Ambulance::all();
         foreach ($ambulances as $key => $ambulance) {
-            $ambulances[$key]->current_case = $ambulance->cases->where('status_available','=',null)->first();
+            $ambulances[$key]->current_case = $ambulance->cases->where('status_available','=',null)->get()->first()->id;
             dd($ambulances[$key]->current_case);
         }
         return response()->json($ambulances);
