d855f73b8a42961ca6dfaa641e31ba1e31ce1b3e
Miguel Santos <github@miguelsantos.tech>
2020-03-23 13:15:41 +0000

    fix(COVID19): migrations

--- database/migrations/2020_03_21_122502_create_covid19_ambulance_cases.php
+++ database/migrations/2020_03_21_122502_create_covid19_ambulance_cases.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCOVID19AmbulanceCase extends Migration
+class CreateCOVID19AmbulanceCases extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2020_03_21_122557_create_covid19_ambulance_team_members.php
+++ database/migrations/2020_03_21_122557_create_covid19_ambulance_team_members.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCOVID19AmbulanceTeam extends Migration
+class CreateCOVID19AmbulanceTeamMembers extends Migration
 {
     /**
      * Run the migrations.
