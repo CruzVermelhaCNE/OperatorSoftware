ccc3de2d63015d046412f2f9ef7b0534ca06f5a2
Miguel Santos <github@miguelsantos.tech>
2020-03-23 18:03:59 +0000

    fix(COVID19): panel issues

--- app/Http/Controllers/COVID19AmbulanceController.php
+++ app/Http/Controllers/COVID19AmbulanceController.php
@@ -67,7 +67,7 @@ class COVID19AmbulanceController extends Controller
 
     public function getTeamMembers($id)
     {
-        $team_members = COVID19AmbulanceTeamMember::where('ambulance_id', '=', $id)->sortByDesc('id')->groupBy('name')->keys();
+        $team_members = COVID19AmbulanceTeamMember::where('ambulance_id', '=', $id)->sort('id','DESC')->groupBy('name')->get()->keys();
         return response()->json($team_members);
     }
 
@@ -77,7 +77,7 @@ class COVID19AmbulanceController extends Controller
         $team_member = COVID19AmbulanceTeamMember::where([
             ['ambulance_id','=',$validated['id']],
             ['name','=',$validated['name']],
-        ])->sortByDesc('id')->groupBy('name')->first();
+        ])->sort('id','desc')->groupBy('name')->get()->first();
         return response()->json($team_member);
     }
 
