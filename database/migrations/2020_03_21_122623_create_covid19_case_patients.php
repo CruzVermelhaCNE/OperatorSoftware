f36a95a1fceb3ea61f661a8405fbf71e111b8ef0
Miguel Santos <github@miguelsantos.tech>
2020-03-23 13:16:24 +0000

    fix(COVID19): fix migration

--- database/migrations/2020_03_21_122623_create_covid19_case_patients.php
+++ database/migrations/2020_03_21_122623_create_covid19_case_patients.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCOVID19AmbulancePatients extends Migration
+class CreateCOVID19CasePatients extends Migration
 {
     /**
      * Run the migrations.
