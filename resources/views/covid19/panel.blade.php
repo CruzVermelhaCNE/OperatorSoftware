d48518db74b760896d2aca213cbc1b2d8b1a82f7
Miguel Santos <github@miguelsantos.tech>
2020-03-23 18:12:27 +0000

    fix(COVID19): fix panel

--- resources/views/covid19/panel.blade.php
+++ resources/views/covid19/panel.blade.php
@@ -1594,16 +1594,18 @@
         $( "#case_team_name" ).blur(function() {
             let ambulance_id = $("#case_team_name").data('ambulance-id');
             let name = $("#case_team_name").val();
-            axios.post("{{route('covid19.ambulance_team_member')}}", {
-                id: ambulance_id,
-                name: name,
-            })
-            .then(function (response) {
-                console.log(response.data);
-            })
-            .catch(function (error) {
-                alert(error);
-            });
+            if(name != "") {
+                axios.post("{{route('covid19.ambulance_team_member')}}", {
+                    id: ambulance_id,
+                    name: name,
+                })
+                .then(function (response) {
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    alert(error);
+                });
+            }
         });
         fetchInitialOpenCases();
         fetchInitialAmbulances();
