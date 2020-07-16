5722f4579275f251c40a3f25a03a4070d3e7145e
Miguel Santos <github@miguelsantos.tech>
2020-07-16 22:03:27 +0100

    fix: timetape all

--- app/Http/Controllers/TheatersOfOperations/TimeTapeController.php
+++ app/Http/Controllers/TheatersOfOperations/TimeTapeController.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers\TheatersOfOperations;
 
 use App\Http\Controllers\Controller;
-use App\TheaterOfOperations;
+use App\TheaterOfOperationsTimeTape;
 
 class TimeTapeController extends Controller
 {
     public function all()
     {
-        $timetapes = TheaterOfOperations::all();
+        $timetapes = TheaterOfOperationsTimeTape::all()->sortByDesc('id');
         return response()->json($timetapes);
     }
 }
