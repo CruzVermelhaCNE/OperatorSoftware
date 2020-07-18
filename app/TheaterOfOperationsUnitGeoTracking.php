157166b90adcebdf2811c9c3cf88e1f4d415850b
Miguel Santos <github@miguelsantos.tech>
2020-07-18 17:00:41 +0100

    debug: wialon

--- app/TheaterOfOperationsUnitGeoTracking.php
+++ app/TheaterOfOperationsUnitGeoTracking.php
@@ -43,7 +43,7 @@ class TheaterOfOperationsUnitGeoTracking extends Model
         $this->long = $long;
         $this->save();
         $this->unit->theater_of_operations->resetUnitsListing();
-        \var_dump($this);
+        \var_dump($this->lat);
         echo("Update GPS Location\n");
         return $this;
     }
