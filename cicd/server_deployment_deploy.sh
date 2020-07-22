e05cc55cb7a2fad3229b1205bdcb3f97ba970942
Miguel Santos <github@miguelsantos.tech>
2020-07-22 16:03:04 +0100

    cicd: implement deployments

---
+++ cicd/server_deployment_deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    git fetch origin deploy
+    git reset --hard origin/deploy
+
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    php artisan migrate --force
+
+    php artisan queue:restart
+    php artisan config:clear
+    php artisan route:clear
+    php artisan cache:clear
+    php artisan view:clear
+
+php artisan up
+
+echo "Application deployed!"
